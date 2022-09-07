378fb589e9e50e381aa75a05b09f2273a3cce737
dlarocque <daniellrq@outlook.com>
2022-09-06 21:29:01 -0500

    temp commit

--- mdp.py
+++ mdp.py
@@ -1,34 +1,54 @@
 import sys
+from io import TextIOWrapper
 import subprocess
-import os
 import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+def compile_to_pdf(filepath: str, out: TextIOWrapper) -> None:
+    print('compiling to pdf')
+    subprocess.run(['pandoc', '-f', 'markdown', '-t',
+                   'pdf', filepath], stdout=out)
 
 
-def main(filename: str):
-    # Validate that filename is a markdown file
+class Handler(FileSystemEventHandler):
+
+    def __init__(self, filepath: str, out: TextIOWrapper):
+        self.filepath = filepath
+        self.out = out
+
+
+    def on_any_event(self, event) -> None:
+        if event.event_type in ['modified', 'deleted']:
+            compile_to_pdf(self.filepath, self.out)
+
+
+
+def main(filepath: str) -> None:
+    # Validate that filepath is a markdown file
     # TODO: Check if file exists
-    if len(filename) <= 3 or filename[-3:] != '.md':
-        print("invalid filename")
+    if len(filepath) <= 3 or filepath[-3:] != '.md':
+        print("invalid filepath")
         return
 
-    out_filename = filename[:-3] + '.pdf'
-    out_file = open(out_filename, 'w')
+    out_filepath = filepath[:-3] + '.pdf'
+    out_file = open(out_filepath, 'w')
 
-    subprocess.run(['pandoc', '-f', 'markdown', '-t',
-                   'pdf', filename], stdout=out_file)
+    compile_to_pdf(filepath, out_file)
 
-    zathura_process = subprocess.Popen(['zathura', out_filename])
+    zathura_process = subprocess.Popen(['zathura', out_filepath])
 
-    last_modified = os.stat(filename).st_mtime
-    while zathura_process.poll() is None:
-        time.sleep(0.1)  # should be event based
+    event_handler = Handler(filepath, out_file)
+    observer = Observer()
+    observer.schedule(event_handler, filepath, recursive=False)
+    observer.start()
 
-        if last_modified != os.stat(filename).st_mtime:
-            last_modified = os.stat(filename).st_mtime
-
-            out_file = open(out_filename, 'w')
-            subprocess.run(['pandoc', '-f', 'markdown', '-t',
-                           'pdf', filename], stdout=out_file)
+    try:
+        while zathura_process.poll() is None:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
 
 
 if __name__ == '__main__':
@@ -36,3 +56,4 @@ if __name__ == '__main__':
         print('missing file name')
     else:
         main(sys.argv[1].strip())
+
